1c7b3a583e10b77b5b99a7d10cab8471daa35de2
JhonellaJazmine <68491417+JhonellaJazmine@users.noreply.github.com>
2020-12-15 16:33:49 +0800

    Update agebracketindentifier.java

--- agebracketindentifier.java
+++ agebracketindentifier.java
@@ -1,3 +1,24 @@
+/***
+Age Bracket Identifier
+by CodeChum
+
+Cody wants to determine if The Programmer is an adult. An adult is anyone aged 18 and above. Input the Programmer's age then print "Adult" if he is an adult and "Minor" if he isn't.
+
+Input Format
+
+Enter a positive or zero integer indicating the Programmer’s age
+
+Input Sample
+
+19
+Output Format
+
+A single line container either "Adult" or "Minor"
+
+Output Sample
+
+Adult    ***/
+
 import java.util.Scanner;
 
 class Main{
